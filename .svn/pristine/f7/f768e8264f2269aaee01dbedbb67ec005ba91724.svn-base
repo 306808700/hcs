d7375dcb80cf0038b7c61ede3f13e528f317b874
hexin <changyuan.lcy@alibaba-inc.com>
2013-11-22 19:12:37 +0800

    hcs

--- .svn/pristine/f7/f768e8264f2269aaee01dbedbb67ec005ba91724.svn-base
+++
@@ -1,6 +0,0 @@
-<?php
-	$attr = $_REQUEST["attr"];
-	$file = file_get_contents("http://www.browsersupport.net/CSS/box-shadow");
-	echo $file;
-
-?>
--- 1/hcs/img.php
+++
@@ -1,8 +0,0 @@
-<?php
-    
-    $jpg =  $_REQUEST["code"];
-    $img = base64_decode(str_replace('data:image/jpeg;base64,', '', $jpg));
-    $filename="".date("h_i_s",time()).".jpg";//要生成的图片名字
-    file_put_contents("../img/".$filename, $img);
-    echo "../img/".$filename;
-?>
